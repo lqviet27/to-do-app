ebd5cd671611868b0d92c6751d0d7c6a245a3400
Le Quoc Viet <lequocviet2752004@gmail.com>
2024-10-31 15:58:52 +0700

    init

---
+++ to-do-list/src/App.js
@@ -0,0 +1,32 @@
+import Signin from './Page/Signin';
+import Signup from './Page/Signup';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { ToastContainer, Bounce } from 'react-toastify';
+function App() {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/signin" element={<Signin />} />
+                <Route path="/signup" element={<Signup />} />
+                <Route path="*" element={<Navigate to="/signin" />} />
+            </Routes>
+
+
+            <ToastContainer
+                position="top-center"
+                autoClose={2000}
+                hideProgressBar={false}
+                newestOnTop={false}
+                closeOnClick
+                rtl={false}
+                pauseOnFocusLoss
+                draggable
+                pauseOnHover
+                theme="light"
+                transition={Bounce}
+            />
+        </BrowserRouter>
+    );
+}
+
+export default App;
